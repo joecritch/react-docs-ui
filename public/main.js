728ff66fcda17d934636de08aeff372fd9090f6e
Joe Critchley <joecritch@gmail.com>
2017-06-29 17:50:01 +0100

    added new code panel design to homepage

---
+++ public/main.js
@@ -0,0 +1,29 @@
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function htmlDecode(input){
+  var e = document.createElement('div');
+  e.innerHTML = input;
+  return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+}
+
+
+
+ready(function() {
+  var editors = document.querySelectorAll('.code');
+  for (var i = 0; i < editors.length; i++) {
+    CodeMirror(editors[i], {
+      value: htmlDecode(editors[i].querySelector('pre').innerHTML),
+      mode: 'jsx',
+      lineNumbers: true,
+      readOnly: editors[i].getAttribute('data-readonly') != null,
+      theme: 'react',
+    });
+    editors[i].querySelector('pre').remove();
+  }
+});
